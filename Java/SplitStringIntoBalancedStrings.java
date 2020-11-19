32290ae41c3f5bae6f37bfcb4e39b5cb1f6d5276
vagmithra123 <vagmithra@gmail.com>
2020-11-19 11:27:21 -0800

    SplitString Into Balanced String

---
+++ Java/SplitStringIntoBalancedStrings.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int balancedStringSplit(String s) {
+        int r = 0; int l = 0; int count = 0;
+        for(int i =0; i< s.length(); i++){
+            if(s.charAt(i) == 'R'){
+                r++;
+            }else{
+                l++;
+            }
+            
+            if(r == l){
+                count++;
+            }
+            
+        }
+       
+        return count;
+    
+        
+    }
+}
