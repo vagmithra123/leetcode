6da1d4952d81b00e3ec7eec3d31e2f82e9bc14e9
vagmithra123 <vagmithra@gmail.com>
2021-01-23 15:58:46 -0800

    Adding all files

---
+++ python/BestTimeToBuyAndSell1.py
@@ -0,0 +1,14 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        l,r = 0, 1 #l = buy, r = sell
+        maxProfit = 0
+        while(r < len(prices)):
+            if(prices[l] < prices[r]):
+                profit = prices[r] - prices[l]
+                maxProfit = max(profit, maxProfit)
+            else:
+                l = r
+            r += 1
+                
+        return maxProfit
+        
