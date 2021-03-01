c0bd43693b6f7ded3bf3d667b6597f4fa705348b
vagmithra123 <vagmithra@gmail.com>
2021-03-01 13:54:38 -0800

    Adding several files

---
+++ python/RunningSum.py
@@ -0,0 +1,27 @@
+'''
+Running sum of 1D Array
+
+Given an array nums. We define a running sum of an array as runningSum[i] = sum(nums[0]…nums[i]).
+
+Return the running sum of nums.
+
+ 
+
+Example 1:
+
+Input: nums = [1,2,3,4]
+Output: [1,3,6,10]
+Explanation: Running sum is obtained as follows: [1, 1+2, 1+2+3, 1+2+3+4].
+
+'''
+
+class Solution:
+    def runningSum(self, nums: List[int]) -> List[int]:
+        runningSum = []
+        for i in range(0,len(nums)):
+            if len(runningSum) != 0:
+                sum = runningSum[-1] + nums[i]
+                runningSum.append(sum)
+            else:
+                runningSum.append(nums[i])
+        return runningSum
