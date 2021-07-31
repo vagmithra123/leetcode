6bd7fcd01c6506464f9c30f01680427a0a89b5b6
vagmithra123 <vagmithra@gmail.com>
2021-07-31 08:54:45 -0700

    Adding RemoveLinkedListElements.py

---
+++ python/RemoveLinkedListElements.py
@@ -0,0 +1,37 @@
+'''
+Given the head of a linked list and an integer val, remove all the nodes of the linked list that has Node.val == val, and return the new head.
+
+ 
+
+Example 1:
+
+
+Input: head = [1,2,6,3,4,5,6], val = 6
+Output: [1,2,3,4,5]
+Example 2:
+
+Input: head = [], val = 1
+Output: []
+Example 3:
+
+Input: head = [7,7,7,7], val = 7
+Output: []
+
+'''
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def removeElements(self, head: ListNode, val: int) -> ListNode:
+        if head is None: # base case
+            return 
+        
+        head.next = self.removeElements(head.next, val)
+        if head.val == val:
+            head = head.next
+                
+        return head
+        
