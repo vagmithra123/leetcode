fbf44d4c25163f193aba8dfaaf33413914c65c0a
vagmithra123 <vagmithra@gmail.com>
2021-08-07 09:37:44 -0700

    Adding one more solution to Reverse String

--- python/Strings/ReverseAString.py
+++ python/Strings/ReverseAString.py
@@ -1,3 +1,32 @@
+'''
+Write a function that reverses a string. The input string is given as an array of characters s.
+
+ 
+
+Example 1:
+
+Input: s = ["h","e","l","l","o"]
+Output: ["o","l","l","e","h"]
+Example 2:
+
+Input: s = ["H","a","n","n","a","h"]
+Output: ["h","a","n","n","a","H"]
+
+
+'''
+
+# Solution1 
+class Solution:
+    def reverseString(self, s: List[str]) -> None:
+        for i in range(len(s)//2):
+            s[i],s[-i-1] = s[-i-1],s[i]
+            
+        
+
+
+
+#Solution2 
+
 class Solution:
     def reverseString(self, s: List[str]) -> None:
         i =0
